63dec3205782acc0fa81ca5c5f88bb51192699cf
sakorn.s <sakorn.s@github.com>
2021-03-02 15:03:49 +0700

    master commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="">
+    <title></title>
+    
+    <!--https://getbootstrap.com/docs/5.0 -->
+    <link href="dist/css/bootstrap.css" rel="stylesheet">
+    <link href="dist/css/blog.css" rel="stylesheet">
+    <link href="dist/fonts/prompt.css" rel="stylesheet">
+
+    <!--https://icons.getbootstrap.com/-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+
+    
+  </head>
+
+<body>    
+<div class="container">
+  <header class="blog-header py-3">
+    <div class="row">
+      <div class="col-12 text-end">
+        <a class="btn btn-lg btn-primary" href="#"><i class="bi bi-cart4"></i> สั่งซื้อ</a>
+      </div>
+    </div>
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-12 text-center">
+        <h1 href="#">ว่านปลาดาว สูตรใหม่</h1>
+      </div>
+    </div>
+  </header>
+
+  <main class="container">
+  
+
+  </main>
+
+  <footer class="blog-footer">
+    <p>Sales Page by <a href="https://www.fdtech.co.th/" target="_blank">FDTECH</a></p>
+   
+  </footer>  
+  </body>
+</html>
